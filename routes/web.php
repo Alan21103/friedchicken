c1c457caa93302ee0885eefbeaf9cf7506d85916
Sagaa4a <127074133+Bayhaqiindra@users.noreply.github.com>
2026-01-05 03:31:46 +0700

    ROUTE FIX

--- routes/web.php
+++ routes/web.php
@@ -24,10 +24,13 @@ Route::middleware('auth.custom:kasir')->group(function () {
     })->name('kasir.index');
 });
 
+// DAPUR //
+
+// Grouping Route Dapur (Gunakan Controller agar data dummy muncul)
 Route::middleware('auth.custom:dapur')->group(function () {
-    Route::get('/dapur', function () {
-        return view('dapur.index');
-    })->name('dapur.index');
+    // Arahkan ke Controller, bukan ke view langsung
+    Route::get('/dapur', [DapurController::class, 'index'])->name('dapur.index');
+    Route::get('/dapur/stok', [DapurController::class, 'stok'])->name('dapur.stok');
 });
 
 /**
